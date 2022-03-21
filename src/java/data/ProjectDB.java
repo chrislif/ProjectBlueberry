aca36a5fcf9372df5ab06a706afd4b08999f4cd2
al725845 <al725845@LR002D02.southeast.edu>
2022-03-21 13:17:44 -0500

    added updateSprintName and updateSprintEndDate

--- src/java/data/ProjectDB.java
+++ src/java/data/ProjectDB.java
@@ -10,6 +10,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Date;
 import model.Sprint;
 
 /**
@@ -17,32 +18,33 @@ import model.Sprint;
  * @author al725845
  */
 public class ProjectDB {
-    public static String createProject (String name, String creationDate) throws SQLException{
+
+    public static String createProject(String name, String creationDate) throws SQLException {
         ConnectionPool pool = ConnectionPool.getInstance();
         Connection connection = pool.getConnection();
         String keyValue = "";
         PreparedStatement statement = null;
         ResultSet resultSet = null;
-        
+
         String query = "INSERT INTO project (projectName, creationDate)"
-                     + "VALUES (?, ?)";
-        
+                + "VALUES (?, ?)";
+
         try {
             statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
             statement.setString(1, name);
             statement.setString(2, creationDate);
-            
+
             statement.executeUpdate();
             resultSet = statement.getGeneratedKeys();
-            
-        } catch (SQLException ex){
+
+        } catch (SQLException ex) {
             throw ex;
         } finally {
             try {
                 if (resultSet != null && statement != null) {
                     resultSet.next();
                     keyValue = resultSet.getString(1);
-                    
+
                     resultSet.close();
                     statement.close();
                 }
@@ -51,56 +53,25 @@ public class ProjectDB {
                 throw ex;
             }
         }
-        return keyValue;   
+        return keyValue;
     }
-    
-    public static void insertContributer (int projectID, int accountID, String tag) throws SQLException {
+
+    public static void insertContributer(int projectID, int accountID, String tag) throws SQLException {
         ConnectionPool pool = ConnectionPool.getInstance();
         Connection connection = pool.getConnection();
         PreparedStatement statement = null;
-        
+
         String query = "INSERT INTO projectPeople (projectID, accountID, tag)"
-                     + "VALUES (?, ?, ?)";
-        
+                + "VALUES (?, ?, ?)";
+
         try {
             statement = connection.prepareStatement(query);
             statement.setInt(1, projectID);
             statement.setInt(2, accountID);
             statement.setString(3, tag);
-            
+
             statement.executeUpdate();
-        } catch (SQLException ex){
-            throw ex;
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-                pool.freeConnection(connection);
-            } catch (SQLException ex) {
-                throw ex;
-            }
-        } 
-    }
-    
-    public static void createSprint (Sprint sprint, int projectID) throws SQLException {
-        ConnectionPool pool = ConnectionPool.getInstance();
-        Connection connection = pool.getConnection();
-        PreparedStatement statement = null;
-        
-        String query = "INSERT INTO sprint (projectID, sprintNum, sprintName, sprintStart, sprintEnd)"
-                     + "VALUES (?, ?, ?, ?, ?)";
-        
-        try {
-            statement = connection.prepareStatement(query);
-            statement.setInt(1, projectID);
-            statement.setInt(2, sprint.getSprintNum());
-            statement.setString(3, sprint.getSprintName());
-            statement.setString(4, sprint.getSprintStartDate());
-            statement.setString(5, sprint.getSprintEndDate());
-            
-            statement.executeUpdate();
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             throw ex;
         } finally {
             try {
@@ -113,4 +84,95 @@ public class ProjectDB {
             }
         }
     }
+
+    public static void createSprint(Sprint sprint, int projectID) throws SQLException {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement statement = null;
+
+        String query = "INSERT INTO sprint (projectID, sprintNum, sprintName, sprintStart, sprintEnd)"
+                + "VALUES (?, ?, ?, ?, ?)";
+
+        try {
+            statement = connection.prepareStatement(query);
+            statement.setInt(1, projectID);
+            statement.setInt(2, sprint.getSprintNum());
+            statement.setString(3, sprint.getSprintName());
+            statement.setString(4, sprint.getSprintStartDate());
+            statement.setString(5, sprint.getSprintEndDate());
+
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            throw ex;
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                pool.freeConnection(connection);
+            } catch (SQLException ex) {
+                throw ex;
+            }
+        }
+    }
+
+    public static int updateSprintName(int sprintID, String name) throws SQLException {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        String query = "UPDATE sprint SET sprintName = ? WHERE sprintID = ?";
+
+        try {
+            statement = connection.prepareStatement(query);
+            statement.setInt(1, sprintID);
+            statement.setString(2, name);
+            
+            return statement.executeUpdate();
+        } catch (SQLException ex) {
+            throw ex;
+        } finally {
+            try {
+                if (resultSet != null && statement != null) {
+                    resultSet.close();
+                    statement.close();
+                }
+                pool.freeConnection(connection);
+            } catch (SQLException e) {
+                throw e;
+            }
+        }
+    }
+
+    public static int updateSprintEnd(int sprintID, Date endDate) throws SQLException {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        String query = "UPDATE sprint SET sprintEnd = ? WHERE sprintID = ?";
+
+        try {
+            statement = connection.prepareStatement(query);
+            statement.setInt(1, sprintID);
+            statement.setDate(2, endDate);
+            
+            return statement.executeUpdate();
+        } catch (SQLException ex) {
+            throw ex;
+        } finally {
+            try {
+                if (resultSet != null && statement != null) {
+                    resultSet.close();
+                    statement.close();
+                }
+                pool.freeConnection(connection);
+            } catch (SQLException e) {
+                throw e;
+            }
+        }
+    }
+    
+    
 }
