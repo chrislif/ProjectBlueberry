9bb9357a63d33fa1c843e2d042c4819221683f16
Jackson Beaudette <jb706443@southeast.edu>
2022-03-23 13:33:46 -0500

    Navigation Controller

--- src/java/controller/Public.java
+++ src/java/controller/Public.java
@@ -54,19 +54,6 @@ public class Public extends HttpServlet {
         ArrayList<String> errorList = new ArrayList();
         
         switch (action) {
-            
-            case "toHome":
-                url = "/index.jsp";
-                break;
-            case "toLogin":
-                url = "/page/auth/login.jsp";
-                break;
-            case "toOverview":
-                url ="/page/project/overview.jsp";
-                break;
-            case "toRegister":
-                url = "/page/auth/register.jsp";
-                break;
             case "register":
                 String accountName = request.getParameter("accountName");
                 password = request.getParameter("password");
